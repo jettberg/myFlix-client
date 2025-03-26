e90faeb49a9dbc2e562a4072377f32cc03dee343
jettberg <164446552+jettberg@users.noreply.github.com>
2025-03-26 11:58:59 -0500

    set up initial code for hooking up my external API

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,7 +1,29 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movieData, onMovieClick }) => {
 
 
     return (
         <div onClick={() => { onMovieClick(movieData); }}>{movieData.title}</div>
     );
+};
+
+
+MovieCard.PropTypes = {
+    movieData: PropTypes.shape({
+
+//This is where you specify the properties of the movieCard view/which will be seen and allowed
+//Also shows how you can nest different elements within any given one
+title: PropTypes.string.isRequired,
+
+image: PropTypes.string.isRequired,
+
+author: PropTypes.string,
+
+genre: PropTypes.shape({
+  name: PropTypes.string,
+})
+
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,70 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 
-// The following is the old code that is used prior to the "useState" state from react
-// export const MainView = () => {
-//     return (
-//       <div>
-//         <div>Eloquent Javascript</div>
-//         <div>Mastering Javascript Functional Programming</div>
-//         <div>Javascript: The Good Parts</div>
-//         <div>Javascript: the Definitive Guide</div>
-//         <div>The Road to React</div>
-//       </div>
-//     );
-//   };
-
-
-
-
-
-
-// the following is the updated code that uses the "useState" prop
 
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1, title: "Eloquent Java",
-      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Jett berg 1"
-    },
+  // const [movies, setMovies] = useState([
+  //   {
+  //     id: 1, title: "Eloquent Java",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+  //     author: "Jett berg 1"
+  //   },
 
 
-    {
-      id: 2, title: "Ugly Java",
-      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Jett berg 2"
-    },
+  //   {
+  //     id: 2, title: "Ugly Java",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+  //     author: "Jett berg 2"
+  //   },
 
 
-    {
-      id: 3, title: "Decent Java",
-      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Jett berg 3"
-    },
+  //   {
+  //     id: 3, title: "Decent Java",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+  //     author: "Jett berg 3"
+  //   },
 
 
-    {
-      id: 4, title: "Whatever Java",
-      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Jett berg 4"
-    },
+  //   {
+  //     id: 4, title: "Whatever Java",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+  //     author: "Jett berg 4"
+  //   },
 
 
-    {
-      id: 5, title: "Astounding Java",
-      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Jett berg 5"
-    }
-  ]);
-
-
-
-
+  //   {
+  //     id: 5, title: "Astounding Java",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+  //     author: "Jett berg 5"
+  //   }
+  // ]);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+
+  useEffect(() => {
+    fetch("https://openlibrary.org/search.json?q=star+wars")
+    .then((response) => response.json())
+    .then((data) => {
+      const moviesFromApi = data.docs.map((doc) => {
+        return {
+          id: doc.key,
+          title: doc.title,
+          image:
+          `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+          author: doc.author_name?.[0]
+        };
+      });
+      setMovies(moviesFromApi);
+    });
+  }, []);
+
+
+
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
